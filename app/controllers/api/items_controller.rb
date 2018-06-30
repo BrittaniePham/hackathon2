47145bab7e298da34666f7816a4079cb2aea9977
Mallory Linton <mallorylinton@gmail.com>
2018-06-30 10:37:36 -0600

    stubbed out the controllers

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,16 +1,41 @@
 class Api::ItemsController < ApplicationController
+  before_action :set_menu, only [:show, :update, :destroy]
+
   def index
+    render json: current_user.items.order("created_at")
   end
 
   def show
+    render json: @item
   end
 
   def create
+    item = current_user.items.new(item_params)
+    if item.save
+      render json: item
+    else
+      render_errors(item)
+    end
   end
 
   def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render_error(@item)
+    end
   end
 
   def destroy
+    @item.destroy
   end
+
+  private
+    def set_item
+      @reservation = current_user.items.find(params[:id])
+    end
+
+    def item_params
+      params.require(:menu).permit(:name, :price, :view, :purchase, :menu_id)
+    end
 end
--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,16 +1,41 @@
 class Api::MenusController < ApplicationController
+  before_action :set_menu, only: [:show, :create, :update, :destroy]
+
   def index
+    render json: current_user.menus.order("created_at")
   end
 
   def show
+    render json: @menu
   end
 
   def create
+    menu = current_user.menus.new(menu_params)
+    if menu.save
+      render json: menu
+    else
+      render_errors(menu)
+    end
   end
 
   def update
+    if @menu.update(menu_params)
+      render json: @menu
+    else
+      render_error(@menu)
+    end
   end
 
   def destroy
+    @menu.destroy
   end
+
+  private
+    def set_menu
+      @menu = current_user.menu.find(params[:id])
+    end
+
+    def menu_params
+      params.require(:menu).permit()
+    end
 end
