ae827f15a3fd525cbaeeaa574d3e1ac56de94a08
Robin Green <greenrj4@gmail.com>
2018-06-30 10:42:42 -0600

    home view

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { 
+  Header, 
+  Image,
+   } from 'semantic-ui-react';
+
 
 class Home extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <Background>
+        <Header as='h1' textAlign='center'>Red House<br/></Header>
+          <Image textAlign='center'src='http://www.redhouseslc.com/pics/food1.jpg'alt="yummy food" height="450px;"  centered/>
+          <br/>
+      </Background>
+
+
+
+
+
     );
   }
 }
+
+const Background = styled.div`
+background: slategray;
+`
 
 export default Home;
