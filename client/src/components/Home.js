5b47420045534c9fd50a78ca83a390e1c653027f
Brittanie Pham <brittaniepham@gmail.com>
2018-06-30 11:12:01 -0600

    logic for category

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import { 
   Header, 
   Image,
-   } from 'semantic-ui-react';
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom'
 
 
 class Home extends Component {
   render() {
     return (
-      <Background>
-        <Header as='h1' textAlign='center'>Red House<br/></Header>
-          <Image textAlign='center'src='http://www.redhouseslc.com/pics/food1.jpg'alt="yummy food" height="450px;"  centered/>
-          <br/>
-      </Background>
-
-
-
+      <Fragment>
+        <Background>
+          <Header as='h1' textAlign='center'>Red House<br/></Header>
+            <Image textAlign='center'src='http://www.redhouseslc.com/pics/food1.jpg'alt="yummy food" height="450px;"  centered/>
+            <br/>
+        </Background>
+      <Link to='/menus/1'>Menu</Link>
+      </Fragment>
 
 
     );
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,54 +1,95 @@
 import React, { Component } from 'react';
-import { Grid, Segment, Header, Card, Divider } from 'semantic-ui-react'
+import { Grid, Segment, Header, Card, Divider, Container } from 'semantic-ui-react'
 import Item from './Item'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+
 
 class Menu extends Component {
   state = { items: [] }
 
   componentDidMount() {
-    axios.get('/api/menus/:id/items')
+    axios.get('/api/menus/1/items')
       .then( res => {
         this.setState({ items: res.data })
       })
   }
 
   render() {
+    const { items } = this.state
     return (
-      <Grid columns='equal'>
-        <Grid.Row>
-          <Grid.Column>
-            <Segment>
-              <Header as='h1' textAlign='center' >Dim Sum</Header>
-              <Divider/>
-            </Segment>
-            <Segment>
-              <Header as='h1' textAlign='center' >Noodle Soups</Header>
-              <Divider/>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>
-              <Header as='h1' textAlign='center' >Stir Fry</Header>
-              <Divider/>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>
-              <Header as='h1' textAlign='center' >Seafood</Header>
-              <Divider/>
-            </Segment>
-            <Segment>
-              <Header as='h1' textAlign='center' >Hot Pot</Header>
-              <Divider/>
-            </Segment>
-            <Segment>
-              <Header as='h1' textAlign='center' >Fried Rice</Header>
-              <Divider/>
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Container>
+        <Grid columns='equal'>
+          <Grid.Row>
+            <Grid.Column>
+              <Segment>
+                <Header as='h1' textAlign='center'>Dim Sum</Header>
+                <Divider/>
+                { items.map (i => 
+                   i.category === 'Dim Sum' &&
+                   <li key={i.id}>
+                    <Link to={`/menus/1/items/${i.id}`} />
+                   </li>
+                )}
+              </Segment>
+              <Segment>
+                <Header as='h1' textAlign='center' >Noodle Soups</Header>
+                <Divider/>
+                { items.map (i => 
+                   i.category === 'Noodle Soups' &&
+                   <li key={i.id}>
+                    <Link to={`/menus/1/items/${i.id}`} />
+                   </li>
+                )}
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment>
+                <Header as='h1' textAlign='center' >Stir Fry</Header>
+                <Divider/>
+                { items.map (i => 
+                   i.category === 'Stir Fry' &&
+                   <li key={i.id}>
+                    <Link to={`/menus/1/items/${i.id}`} />
+                   </li>
+                )}
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment>
+                <Header as='h1' textAlign='center' >Seafood</Header>
+                <Divider/>
+                { items.map (i => 
+                   i.category === 'Seafood' &&
+                   <li key={i.id}>
+                    <Link to={`/menus/1/items/${i.id}`} />
+                   </li>
+                )}
+              </Segment>
+              <Segment>
+                <Header as='h1' textAlign='center' >Hot Pot</Header>
+                <Divider/>
+                { items.map (i => 
+                   i.category === 'Hot Pot' &&
+                   <li key={i.id}>
+                    <Link to={`/menus/1/items/${i.id}`} />
+                   </li>
+                )}
+              </Segment>
+              <Segment>
+                <Header as='h1' textAlign='center' >Fried Rice</Header>
+                <Divider/>
+                { items.map (i => 
+                   i.category === 'Fried Rice' &&
+                   <li key={i.id}>
+                    <Link to={`/menus/1/items/${i.id}`} />
+                   </li>
+                )}
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
