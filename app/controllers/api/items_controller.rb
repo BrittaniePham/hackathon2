735dc16e6555e1302441442208452380e74d33e5
Brittanie Pham <brittaniepham@gmail.com>
2018-06-30 11:17:20 -0600

    idk man

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,5 +1,5 @@
 class Api::ItemsController < ApplicationController
-  before_action :set_menu, only [:show, :update, :destroy]
+  before_action :set_menu, only: [:show, :update, :destroy]
 
   def index
     render json: Menu.items.order("created_at")
